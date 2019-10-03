ca38f9b1083de3675a3135fbdbd816794d42b65e
Konrad Czarnota <konrad.czarnota@tooploox.com>
2019-10-03 11:03:35 +0200

    Second task done

--- hw1_release/filters.py
+++ hw1_release/filters.py
@@ -29,7 +29,22 @@ def conv_nested(image, kernel):
     out = np.zeros((Hi, Wi))
 
     ### YOUR CODE HERE
-    pass
+    if kernel.shape[0] > image.shape[0] or kernel.shape[1] > image.shape[1]:
+    	print('Kernel is bigger than image! Provide arguments in swap order.')
+    	return image
+
+    flipped = kernel[::-1, ::-1]
+    # skip edges as there was no information on which padding should be used
+    row_moves = kernel.shape[0] // 2
+    column_moves = kernel.shape[1] // 2
+    for r in range(row_moves, image.shape[0] - row_moves):
+    	for c in range(column_moves, image.shape[1] - column_moves):
+    		sum_v = 0
+    		for i in range(-row_moves, row_moves + 1):
+    			for j in range(-column_moves, column_moves + 1):
+    				sum_v += image[r + i, c + j] * flipped[i + row_moves, j + column_moves]
+    		out[r, c] = sum_v
+
     ### END YOUR CODE
 
     return out
@@ -56,7 +71,8 @@ def zero_pad(image, pad_height, pad_width):
     out = None
 
     ### YOUR CODE HERE
-    pass
+    out = np.zeros((H + 2 * pad_height, W + 2 * pad_width))
+    out[pad_height : H + pad_height, pad_width : W + pad_width] = image
     ### END YOUR CODE
     return out
 
@@ -85,7 +101,15 @@ def conv_fast(image, kernel):
     out = np.zeros((Hi, Wi))
 
     ### YOUR CODE HERE
-    pass
+    row_pad = kernel.shape[0] // 2
+    column_pad = kernel.shape[1] // 2
+    padded = zero_pad(image, row_pad, column_pad)
+    flipped = kernel[::-1, ::-1]
+
+    for r in range(row_pad, padded.shape[0] - row_pad):
+    	for c in range(column_pad, padded.shape[1] - column_pad):
+    		out[r - row_pad, c - column_pad] = np.sum(padded[r - row_pad : r + row_pad + 1,
+    		 c - column_pad : c + column_pad + 1] * flipped)
     ### END YOUR CODE
 
     return out
@@ -104,7 +128,11 @@ def conv_faster(image, kernel):
     out = np.zeros((Hi, Wi))
 
     ### YOUR CODE HERE
-    pass
+    fft_size = [Hi+Hk-1,Wi+Wk-1]
+    fft_img = np.fft.fft2(image,fft_size)
+    fft_ker = np.fft.fft2(kernel,fft_size)
+    conv_img = np.fft.ifft2(fft_img*fft_ker).real
+    out = conv_img[Hk//2:conv_img.shape[0]-Hk//2,Wk//2:conv_img.shape[1]-Wk//2]
     ### END YOUR CODE
 
     return out
@@ -123,8 +151,9 @@ def cross_correlation(f, g):
     """
 
     out = None
+    flipped_g = g[::-1, ::-1]
     ### YOUR CODE HERE
-    pass
+    out = conv_fast(f, flipped_g)
     ### END YOUR CODE
 
     return out
@@ -146,7 +175,8 @@ def zero_mean_cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
+    g_m = g - g.mean()
+    out = cross_correlation(f, g_m)
     ### END YOUR CODE
 
     return out
@@ -170,7 +200,17 @@ def normalized_cross_correlation(f, g):
 
     out = None
     ### YOUR CODE HERE
-    pass
+    out = np.zeros(f.shape)
+    row_pad = g.shape[0] // 2
+    column_pad = g.shape[1] // 2
+    padded = zero_pad(f, row_pad, column_pad)
+    kernel = (g - g.mean()) / g.std()
+
+    for r in range(row_pad, padded.shape[0] - row_pad):
+    	for c in range(column_pad, padded.shape[1] - column_pad):
+    		patch = padded[r - row_pad : r + row_pad + 1, c - column_pad : c + column_pad + 1]
+    		patch = (patch - patch.mean())/ patch.std()
+    		out[r - row_pad, c - column_pad] = np.sum(patch * kernel)
     ### END YOUR CODE
 
     return out
